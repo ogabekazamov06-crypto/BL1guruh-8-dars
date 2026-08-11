6311c8c7ce1b6599aea64980142b09bcd7c90d1b
Ogabek <ogabekazamov06@gmail.com>
2026-08-11 08:55:02 +0500

    2-kodim tugadi

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-with open("dostlar", "w", encoding="utf-8") as f:
-    dost = input("Ismingizni kiriting: ")
-    f.write(dost + "\n")
-print("dostlaringz ismi saqlandi")
+# with open("dostlar", "w", encoding="utf-8") as f:
+#     dost = input("Ismingizni kiriting: ")
+#     f.write(dost + "\n")
+# print("dostlaringz ismi saqlandi")
+
+meva = ["olma", "anor"]
+for i in meva:
+    print(f"Man {i} ni yaxshi koramn")
